cd11f7f95b49f8f91609d65c7b145fd573ef2c2a
FelipeAlves-25 <felipefas209@outlook.com>
2021-09-16 18:50:31 -0300

    Testado a interface (resultado: em funcionamento)

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
 # Janela
 
+class Janela(BoxLayout):
+  pass
+
+class Construtor(App):
+  def build(self):
+    return Janela()
+
+Construtor().run()
 # IA
 
 # Eventos
